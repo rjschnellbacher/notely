9ea78f1b00f1b70c1b61f59d189b2e1a5ca7b33d
Ron Schnellbacher <rjschnell.tech@sbcglobal.net>
2015-05-07 10:39:31 -0400

    fixed plural in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validate :password, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }
   # validate that password is at least 8 characters
 end
